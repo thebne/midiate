b98e605142b7ea5b68cbd51aa8fc7225f0c4ed24
Meital Avitan <meitala@berkeley.edu>
2020-03-20 22:56:04 +0200

    working version, needs refactor and cleanup

--- input.py
+++ input.py
@@ -18,11 +18,11 @@ socket.bind(INPUT_ZMQ_URL)
 def stream_live(device_name):
     print("Streaming from", device_name, "(live)")
     with mido.open_input(device_name) as port:
-        t1 = time.perf_counter_ns()
+        t1 = None
         for msg in port:
             t2 = time.perf_counter_ns()
             print(msg)
-            socket.send(msg.bin() + struct.pack("<Q", t2 - t1))
+            socket.send(msg.bin() + struct.pack("<Q", t2 - t1 if t1 is not None else 0))
             t1 = t2
 
 
@@ -30,10 +30,17 @@ def stream_file(file_path, loop):
     print("Streaming from", file_path, "(file)")
     while True:
         mid = mido.MidiFile(file_path)
-        t1 = time.perf_counter_ns()
+
+        for i, track in enumerate(mid.tracks):
+            print('Track {}: {}'.format(i, track.name))
+            for msg in track:
+                print(msg)
+
+
+        t1 = None
         for msg in mid.play():
             t2 = time.perf_counter_ns()
-            socket.send(msg.bin() + struct.pack("<Q", t2 - t1))
+            socket.send(msg.bin() + struct.pack("<Q", t2 - t1 if t1 is not None else 0))
             t1 = t2
 
         if not loop:
--- recorder.py
+++ recorder.py
@@ -1,27 +1,45 @@
 import zmq
+import sys
 import struct
+from datetime import datetime 
 from time import perf_counter
-from mido import Message, MidiFile, MidiTrack
+from mido import Message, MidiFile, MidiTrack, MetaMessage, second2tick
 
 HOST = "127.0.0.1"
 INPUT_ZMQ_URL = f"tcp://{HOST}:20300"
-
-ctx = zmq.Context()
-sock = ctx.socket(zmq.SUB)
-sock.connect(INPUT_ZMQ_URL)
-sock.subscribe(b'')
+NANOSECONDS_IN_SECONDS = 10**9
 
 mid = MidiFile()
-track = MidiTrack()
-mid.tracks.append(track)
 
-prev_ts = 0
+def main():
+    ctx = zmq.Context()
+    sock = ctx.socket(zmq.SUB)
+    sock.connect(INPUT_ZMQ_URL)
+    sock.subscribe(b'')
 
-while True:
-    data = sock.recv()
-    msg, current_ts = data[0:3], data[3:]
-    import pdb; pdb.set_trace()
-    current_ts, = struct.unpack("<Q", current_ts)
-    import pbd; pbd.set_trace()
-    msg.time = mido.second2tick(current_ts - prev_ts, 480, 500000) 
-    track.append(msg)
+    track = MidiTrack()
+    track.append(MetaMessage('track_name', name=f'MIDIate Recorder {str(datetime.now())}', time=0))
+    track.append(MetaMessage('set_tempo', tempo=500000, time=0))
+    track.append(MetaMessage('time_signature', numerator=4, denominator=4, clocks_per_click=24, notated_32nd_notes_per_beat=8, time=0))
+    mid.tracks.append(track)
+
+    while True:
+        print('Recording...')
+        data = sock.recv()
+        msg, current_ts = data[0:3], data[3:]
+        current_ts, = struct.unpack("<Q", current_ts)
+        print(f'current_ts is {current_ts}')
+        msg = Message.from_bytes(msg) 
+        msg.time = int(second2tick(current_ts / NANOSECONDS_IN_SECONDS, 480, 500000))
+        track.append(msg)
+        print('Msg: ' ,msg)
+        # import pdb; pdb.set_trace()
+
+try:
+    main()
+except KeyboardInterrupt:
+    print('Recorder stopped')
+    for msg in mid: print (msg)
+    mid.save('samples/special_song2.mid')
+    sys.exit()    
+
