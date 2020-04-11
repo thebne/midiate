b9ab24bf884c34096309288dc6f659e6a88e3d23
Omer Perry <thebne@gmail.com>
2020-04-11 19:56:23 +0300

    treat note_on velocity=0 as note_off

--- client/src/redux/reducers/events.js
+++ client/src/redux/reducers/events.js
@@ -21,9 +21,14 @@ function detectChord(notes) {
 
 function parseMessage(rawMsg) {
   let msg = MIDIMessage({data: new Uint8Array(rawMsg)})
+  // enrich with note and frequency
   if (msg.key !== undefined) {
     msg.note = Midi.midiToNoteName(msg.key)
     msg.freq = Midi.midiToFreq(msg.key)
+  }
+  // treat note_on with velocity=0 as note_off
+  if (msg.messageType === 'noteon' && msg.velocity === 0) {
+    msg.messageType = 'noteoff'
   }
   return msg
 }
