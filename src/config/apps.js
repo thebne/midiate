386299e26b9d94428411b78735d96e0c993d117d
operry <operry@magicleap.com>
2020-05-05 01:16:03 +0300

    remove app registries

--- src/config/apps.js
+++ src/config/apps.js
@@ -1,7 +1,7 @@
 export default [
-  require('../apps/chord-recognizer/src'),
-  require('../apps/piano-simulator/src'),
-  require('../apps/heatmap/src'),
-  require('../apps/midi-output/src'),
-  require('../apps/shortcuts/src'),
+  require('../apps/chord-recognizer'),
+  require('../apps/piano-simulator'),
+  require('../apps/heatmap'),
+  require('../apps/midi-output'),
+  require('../apps/shortcuts'),
 ]
